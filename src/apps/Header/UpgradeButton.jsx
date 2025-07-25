0e6ed02e86622ed786e3368b8587f5d5833677b6
govindweb777 <prajapatigovind866@gmail.com>
2025-07-25 14:04:19 +0530

    fix

--- src/apps/Header/UpgradeButton.jsx
+++ src/apps/Header/UpgradeButton.jsx
@@ -22,7 +22,7 @@ export default function UpgradeButton() {
         }}
         icon={<RocketOutlined />}
         onClick={() => {
-          window.open(`https://cloud.idurarapp.com`);
+          window.open(``);
         }}
       >
         {translate('Try Entreprise Version')}
@@ -32,5 +32,5 @@ export default function UpgradeButton() {
 }
 
 console.log(
-  '🚀 Welcome to IDURAR ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
+  '🚀 Welcome to Webseeder ERP CRM! Did you know that we also offer commercial customization services? Contact us at hello@idurarapp.com for more information.'
 );
